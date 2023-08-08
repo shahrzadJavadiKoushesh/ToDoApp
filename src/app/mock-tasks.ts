3032c5eeeff4bfb6001f6d1176c235b9ddfe9664
shahrzad <shahrzadjkoushesh@gmail.com>
2023-08-08 14:07:24 +0330

    add Task model

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,24 @@
+import { Task } from "./Task"
+export const TASKS: Task[ ] = [
+    {
+        id: 1, 
+        text: "Doctors Appointment",
+        day: "May 5th at 2:30pm", 
+        reminder: true
+    },{
+        id: 2, 
+        text: "Doctors Appointment",
+        day: "May 5th at 2:30pm", 
+        reminder: true
+    },{
+        id: 3, 
+        text: "Doctors Appointment",
+        day: "May 5th at 2:30pm", 
+        reminder: true
+    },{
+        id: 4, 
+        text: "Doctors Appointment",
+        day: "May 5th at 2:30pm", 
+        reminder: true
+    },
+]
