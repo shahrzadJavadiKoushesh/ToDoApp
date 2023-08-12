32fbfe9e2c3caeecaf62bafc998d395b6ac602e5
shahrzad <shahrzadjkoushesh@gmail.com>
2023-08-12 10:02:38 +0330

    get data from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http'
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,17 +2,20 @@ import { Injectable } from '@angular/core';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
 import {Observable, of} from 'rxjs'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
 
   getTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
